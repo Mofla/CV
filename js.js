27b8a00d8f187e33303c7a973d903be1330fc70e
Florent Maillard <florent.maillard.pro@gmail.com>
2016-04-15 16:46:15 +0200

    Ajout du js

---
+++ js.js
@@ -0,0 +1,10 @@
+function montrerDiv(id){
+	document.getElementById(id).style.visibility = "visible";
+}
+
+function cacheDiv(id){
+	document.getElementById(id).style.visibility = "hidden";
+}	
+function Couleur(color){
+	document.body.style.background = color;
+}
